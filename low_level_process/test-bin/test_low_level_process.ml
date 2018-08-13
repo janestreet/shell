2d4a48ceb4356cc1d235b416107a5e4a95650d2b
Xavier Clerc <xclerc@janestreet.com>
2018-08-13 10:25:18 +0100

    v0.11.117.19+179

--- low_level_process/test-bin/test_low_level_process.ml
+++ low_level_process/test-bin/test_low_level_process.ml
@@ -10,7 +10,7 @@ let test =
        match
          Low_level_process.run
            ~prog:"bash"
-           ~args:["-c"; "trap '' TERM; echo hello; sleep 1000"]
+           ~args:["-c"; "trap '' TERM; echo hello; sleep 46"]
            ~stdoutf:(fun s len ->
              if String.mem (Bytes.To_string.subo ~len s) '\n'
              then raise Got_the_first_line)
